d488a3f6eddd376d6dd91d804cf51c92f7cc02d1
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2015-12-02 02:09:03 +0100

    6. Bind one object

--- app.js
+++ app.js
@@ -31,7 +31,17 @@ if (Meteor.isClient) {
 		};
 	});
 	
-	angular.module('socially').controller('PartyDetailsCtrl', function($scope, $stateParams) {
-		$scope.partyId = $stateParams.partyId;
+	angular.module('socially').controller('PartyDetailsCtrl', function($scope, $stateParams, $meteor) {
+		$scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+		$scope.save = function() {
+			$scope.party.save().then(function(numberOfDocs) {
+				console.log('Save success doc affected', numberOfDocs);
+			}, function(error) {
+				console.log('Save error', error);
+			});
+		};
+		$scope.reset = function() {
+			$scope.party.reset();
+		};
 	});
 }
