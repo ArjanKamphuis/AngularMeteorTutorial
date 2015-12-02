5def74b248e9ddae429272402aae9b9b87995b2e
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2015-12-02 01:30:25 +0100

    4. Adding/removing objects and Angular event handling

--- app.js
+++ app.js
@@ -3,5 +3,15 @@ if (Meteor.isClient) {
 	
 	angular.module('socially').controller('PartiesListCtrl', function($scope, $meteor) {
 		$scope.parties = $meteor.collection(Parties);
+		$scope.add = function() {
+			$scope.parties.save($scope.newParty);
+			$scope.newParty = '';
+		};
+		$scope.remove = function(party) {
+			$scope.parties.remove(party);
+		};
+		$scope.removeAll = function() {
+			$scope.parties.remove();
+		};
 	});
 }
